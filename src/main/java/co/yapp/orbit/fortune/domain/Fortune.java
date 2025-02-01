99f294a3244eda6917b9056fe1abf222babb5b93
yesjjin99 <yesjjin99@naver.com>
2025-02-01 23:56:57 +0900

    feat: add fortune, fortuneItem domain
    YAF-102

--- src/main/java/co/yapp/orbit/fortune/domain/Fortune.java
+++ src/main/java/co/yapp/orbit/fortune/domain/Fortune.java
@@ -1,5 +1,101 @@
 package co.yapp.orbit.fortune.domain;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import java.util.Objects;
+import lombok.Getter;
+
+@Getter
 public class Fortune {
 
+    private final String dailyFortune;
+    private final int avgFortuneScore;
+
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "score", column = @Column(name = "STUDY_CAREER_SCORE")),
+        @AttributeOverride(name = "description", column = @Column(name = "STUDY_CAREER_DESCRIPTION"))
+    })
+    private final FortuneItem studyCareerFortune;
+
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "score", column = @Column(name = "WEALTH_SCORE")),
+        @AttributeOverride(name = "description", column = @Column(name = "WEALTH_DESCRIPTION"))
+    })
+    private final FortuneItem wealthFortune;
+
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "score", column = @Column(name = "HEALTH_SCORE")),
+        @AttributeOverride(name = "description", column = @Column(name = "HEALTH_DESCRIPTION"))
+    })
+    private final FortuneItem healthFortune;
+
+    @Embedded
+    @AttributeOverrides({
+        @AttributeOverride(name = "score", column = @Column(name = "LOVE_SCORE")),
+        @AttributeOverride(name = "description", column = @Column(name = "LOVE_DESCRIPTION"))
+    })
+    private final FortuneItem loveFortune;
+
+    private final String luckyOutfitTop;
+    private final String luckyOutfitBottom;
+    private final String luckyOutfitShoes;
+    private final String luckyOutfitAccessory;
+
+    private final String unluckyColor;
+    private final String luckyColor;
+    private final String luckyFood;
+
+    public Fortune(String dailyFortune, int avgFortuneScore, FortuneItem studyCareerFortune,
+        FortuneItem wealthFortune, FortuneItem healthFortune, FortuneItem loveFortune,
+        String luckyOutfitTop, String luckyOutfitBottom, String luckyOutfitShoes,
+        String luckyOutfitAccessory, String unluckyColor, String luckyColor, String luckyFood) {
+        this.dailyFortune = dailyFortune;
+        this.avgFortuneScore = avgFortuneScore;
+        this.studyCareerFortune = studyCareerFortune;
+        this.wealthFortune = wealthFortune;
+        this.healthFortune = healthFortune;
+        this.loveFortune = loveFortune;
+        this.luckyOutfitTop = luckyOutfitTop;
+        this.luckyOutfitBottom = luckyOutfitBottom;
+        this.luckyOutfitShoes = luckyOutfitShoes;
+        this.luckyOutfitAccessory = luckyOutfitAccessory;
+        this.unluckyColor = unluckyColor;
+        this.luckyColor = luckyColor;
+        this.luckyFood = luckyFood;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Fortune fortune = (Fortune) o;
+        return getAvgFortuneScore() == fortune.getAvgFortuneScore() && Objects.equals(
+            getDailyFortune(), fortune.getDailyFortune()) && Objects.equals(
+            getStudyCareerFortune(), fortune.getStudyCareerFortune()) && Objects.equals(
+            getWealthFortune(), fortune.getWealthFortune()) && Objects.equals(
+            getHealthFortune(), fortune.getHealthFortune()) && Objects.equals(
+            getLoveFortune(), fortune.getLoveFortune()) && Objects.equals(getLuckyOutfitTop(),
+            fortune.getLuckyOutfitTop()) && Objects.equals(getLuckyOutfitBottom(),
+            fortune.getLuckyOutfitBottom()) && Objects.equals(getLuckyOutfitShoes(),
+            fortune.getLuckyOutfitShoes()) && Objects.equals(getLuckyOutfitAccessory(),
+            fortune.getLuckyOutfitAccessory()) && Objects.equals(getUnluckyColor(),
+            fortune.getUnluckyColor()) && Objects.equals(getLuckyColor(),
+            fortune.getLuckyColor()) && Objects.equals(getLuckyFood(),
+            fortune.getLuckyFood());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getDailyFortune(), getAvgFortuneScore(), getStudyCareerFortune(),
+            getWealthFortune(), getHealthFortune(), getLoveFortune(), getLuckyOutfitTop(),
+            getLuckyOutfitBottom(), getLuckyOutfitShoes(), getLuckyOutfitAccessory(),
+            getUnluckyColor(),
+            getLuckyColor(), getLuckyFood());
+    }
 }
---
+++ src/main/java/co/yapp/orbit/fortune/domain/FortuneItem.java
@@ -0,0 +1,35 @@
+package co.yapp.orbit.fortune.domain;
+
+import jakarta.persistence.Embeddable;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+public class FortuneItem {
+
+    private int score;
+    private String description;
+
+    public FortuneItem(int score, String description) {
+        this.score = score;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FortuneItem that = (FortuneItem) o;
+        return getScore() == that.getScore() && Objects.equals(getDescription(),
+            that.getDescription());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getScore(), getDescription());
+    }
+}
