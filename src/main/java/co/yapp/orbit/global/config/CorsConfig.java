f853877527e9a152ce1ab6fd863f2c4e0f8a59e7
민병욱 <byungwook-min@naver.com>
2025-02-02 20:42:53 +0900

    [YAF-106] Introduce CorsConfig for CORS configuration (#9)
    * feat: add global CORS configuration 
    - Introduce CorsConfig class to configure CORS settings for the application.
    - Allow requests from "https://www.orbitalarm.net" with methods GET, POST, PUT, DELETE, and OPTIONS.
    - Enable all headers and credentials for cross-origin requests.
    
    YAF-106

---
+++ src/main/java/co/yapp/orbit/global/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package co.yapp.orbit.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://www.orbitalarm.net")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .allowCredentials(true);
+    }
+}
